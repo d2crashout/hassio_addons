0e2a3d41353f7d56312342b544d84da9586615f1
Abhimanyu <methebestestofall@gmail.com>
2025-05-06 13:49:40 -0400

    hopefully fixed config

--- inspircd/run.sh
+++ inspircd/run.sh
@@ -17,22 +17,56 @@ fi
 # 3) Overwrite with a minimal valid inspircd.conf
 cat > "$CONFIG_DIR/inspircd.conf" <<EOF
 <inspircd>
-<server name="${SERVER_NAME}" description="${SERVER_DESCRIPTION}" network="${NETWORK_NAME}" id="1" hidden="no" />
-<admin name="${ADMIN_NAME}" email="${ADMIN_EMAIL}" location="${ADMIN_LOCATION}" />
-<listen address="0.0.0.0" port="${LISTEN_PORT}" options="ipv4" />
-<listen address="0.0.0.0" port="6697" options="ssl" sslcertfile="${CONFIG_DIR}/server.pem" sslkeyfile="${CONFIG_DIR}/server.key" />
+
+<server
+  name="${SERVER_NAME}"
+  description="${SERVER_DESCRIPTION}"
+  network="${NETWORK_NAME}"
+  id="1"
+  hidden="no"
+/>
+
+<admin
+  name="${ADMIN_NAME}"
+  email="${ADMIN_EMAIL}"
+  location="${ADMIN_LOCATION}"
+/>
+
+<bind
+  address="0.0.0.0"
+  port="${LISTEN_PORT}"
+  type="clients"
+/>
+<bind
+  address="0.0.0.0"
+  port="6697"
+  type="clients"
+  options="ssl"
+  sslcertfile="${CONFIG_DIR}/server.pem"
+  sslkeyfile="${CONFIG_DIR}/server.key"
+/>
+
 <class name="default">
   <pingfreq>120</pingfreq>
   <sendq>100000</sendq>
   <recvq>8000</recvq>
   <timeout>45</timeout>
 </class>
+
 <connect class="default" />
-<oper name="${ADMIN_NAME}" password="${ADMIN_PASSWORD}" class="default" flags="oO" />
+
+<oper
+  name="${ADMIN_NAME}"
+  password="${ADMIN_PASSWORD}"
+  class="default"
+  flags="oO"
+/>
+
 <motd>
 :Welcome to ${NETWORK_NAME}!
 :Server: ${SERVER_NAME}
 </motd>
+
 </inspircd>
 EOF
 
